14f1493e21a0881844f0f33a71769baadadbe67d
Daniel Samuel <dansam5000@gmail.com>
2021-07-07 23:30:07 +0100

    Add function declaration solution in js

--- javascript-soln/fibonacci-sum.js
+++ javascript-soln/fibonacci-sum.js
@@ -16,20 +16,20 @@ const sumFibs = (sum) => {
 }
 
 
-// function sumFibs(num) {
-//     // write your code here
-//     let firstNum = 0;
-//     let secondNum = 1;
-//     let nextNum;
-//     let sumOdd = 0;
-//     for(let i = 0; i <= num; i++){
-//       nextNum = firstNum + secondNum;
-//       firstNum = secondNum;
-//       secondNum = nextNum;
-//       if(nextNum%2 !== 0 && nextNum <= num){
-//         sumOdd = nextNum + sumOdd
-//       }
-//     } 
-//     return sumOdd + 1
-//   }
+function sumFibs(num) {
+    // write your code here
+    let firstNum = 0;
+    let secondNum = 1;
+    let nextNum;
+    let sumOdd = 0;
+    for(let i = 0; i <= num; i++){
+      nextNum = firstNum + secondNum;
+      firstNum = secondNum;
+      secondNum = nextNum;
+      if(nextNum%2 !== 0 && nextNum <= num){
+        sumOdd = nextNum + sumOdd
+      }
+    } 
+    return sumOdd + 1
+  }
   
